741a43ab8bdf087492867a53759878dc38ba9172
donspb <donspb@gmail.com>
2021-07-13 07:47:07 +0300

    Practice 3

---
+++ app/src/main/java/ru/gb/donspb/justpic/ui/addon/AddonActivity.kt
@@ -0,0 +1,14 @@
+package ru.gb.donspb.justpic.ui.addon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.donspb.justpic.R
+
+class AddonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addon)
+
+    }
+}
